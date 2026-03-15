e1c5e246abdabe52e3a013ee18448b0d7097abfd
Device21 <Device198356@gmail.com>
2026-03-15 20:03:51 +0200

    Update and rename source.luau to source.lua

--- source.lua
+++ source.lua
@@ -191,7 +191,7 @@ GlobalWindow.Parent = CoreGui;
 NeverLose.Scales = {
 	Small = UDim2.fromOffset(540,380),
 	Mobile = UDim2.fromOffset(640,385),
-	Default = UDim2.fromOffset(640 , 480),
+	Default = UDim2.fromOffset(780 , 580),
 	Large = UDim2.fromOffset(800 , 600)
 };
 
